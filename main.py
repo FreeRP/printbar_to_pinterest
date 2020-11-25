14f940b7b579485a234a5d7a89ce6725bd47434e
Alexey <math1993@mail.ru>
2020-11-25 20:50:38 +0300

    Add exception requests.exceptions.HTTPError

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
     authorization_data = load_json('config/login.json')
     board_and_url = load_json('config/board_data.json')
     pin_uploader = PinUploader(authorization_data)
+    pin_uploader.login()
     printbar_parser = PrintbarParser()
     for pinterest_board_name, printbar_catalog_url in board_and_url.items():
         board_id = pin_uploader.get_board_id(pinterest_board_name)
@@ -15,4 +16,8 @@ def main():
             pin_uploader.create_pin(board_id, name, price, image_path, printbar_url)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except requests.exceptions.HTTPError:
+        sleep(300)
+        main()
