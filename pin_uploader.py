c446a38056277ac6b5a7df92267c4c7eb369aa36
Alexey <math1993@mail.ru>
2020-11-26 20:41:34 +0300

    Refactor pin_uploader.py
    Removed try ... catch statement from login()
    Removed get_proxies()

--- pin_uploader.py
+++ pin_uploader.py
@@ -1,45 +1,33 @@
-import csv
-import requests
 from time import sleep
+import requests
 from py3pin.Pinterest import Pinterest
-
-from utils import load_json
 
 
 class PinUploader:
 
-    def __init__(self, cfg):
+    def __init__(self, cfg, logger):
         self.__cfg = cfg
         self.__pinterest = None
-
-    @staticmethod
-    def __get_proxies():
-        return {}
+        self.__logger = logger
 
     def login(self):
         self.__pinterest = Pinterest(email=self.__cfg['email'],
                                      password=self.__cfg['password'],
                                      username=self.__cfg['username'],
                                      cred_root=self.__cfg['cred_root'])
-        try:
-            self.__pinterest.login()
-        except:
-            proxies = self.__get_proxies()
-            self.__pinterest = Pinterest(email=self.__cfg['email'],
-                                         password=self.__cfg['password'],
-                                         username=self.__cfg['username'],
-                                         cred_root=self.__cfg['cred_root'],
-                                         proxies=proxies)
+        self.__logger.info('logged in successfully')
 
     def logout(self):
         self.__pinterest.logout()
+        self.__logger.info('log out')
 
     def get_board_id(self, boardname):
         response = None
         try:
             response = self.__pinterest.create_board(name=boardname)
         except requests.exceptions.HTTPError as err:
-            return None
+            self.__logger.exception(err)
+        self.__logger.exception('board created')
         return response.json()['resource_response']['data']['id']
 
     def create_pin(self, board_id, title, price, image_path, printbar_url):
@@ -47,9 +35,9 @@ class PinUploader:
                          Выберите тип ткани и вид печати.
                          Продукция будет готова через 48 часов.'''
         hashtag = title.replace(' ', ' #')
-        self.__pinterest.upload_pin(board_id=board_id,
-                            image_file=image_path,
+        self.__pinterest.pin(board_id=board_id,
+                            image_url=image_path,
                             description= f"Цена: {price}. {description} {hashtag}",
                             title=title,
                             link=printbar_url)
-
+        self.__logger.info('pin created')
