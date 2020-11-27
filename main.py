9f374fb8f3eb36c7f0f181c8a33405b0ad969217
Alexey <math1993@mail.ru>
2020-11-27 15:29:44 +0300

    Reformat main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 import requests
 
 from pin_uploader import PinUploader
@@ -14,15 +15,21 @@ def main():
     pin_uploader.login()
 
     printbar_parser = PrintbarParser(logger)
-    for pinterest_board_name, printbar_catalog_url in board_and_url.items():
+    for pinterest_board_name, printbar_url in board_and_url.items():
         board_id = pin_uploader.get_board_id(pinterest_board_name)
-        products_data = printbar_parser.get_products_data(printbar_catalog_url['url'])
+        products_data = printbar_parser.get_products_data(printbar_url['url'])
         for name, price, image_path, printbar_url in products_data:
             try:
                 pin_uploader.create_pin(board_id, name, price, image_path, printbar_url)
             except requests.exceptions.HTTPError as err:
                 logger.exception(err)
-    pin_uploader.logout()
+                pin_uploader.logout()
+                raise RuntimeError('Error')
+            except Exception as err:
+                logger.exception(f'unknow exception: {err}')
+                raise RuntimeError('Error')
+            sleep(10)
+
 
 if __name__ == "__main__":
     main()
