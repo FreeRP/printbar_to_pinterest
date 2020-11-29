24c7aa6c80e2e89c9fc2d8f90443e760f593bb4c
Alexey <math1993@mail.ru>
2020-11-29 20:54:40 +0300

    add to log message module name

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 import json
 import logging
 
+
 def get_logger(log_file, name):
     logger = logging.getLogger(name)
     file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(logging.INFO)
-    file_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setLevel(logging.DEBUG)
+    file_format = logging.Formatter('%(asctime)s -%(module)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(file_format)
     logger.addHandler(file_handler)
     return logger
